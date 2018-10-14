7cdefa22c8d006c80912f892d1a8d38103b65f6a
Sheldon <sbmusicfreak15@msn.com>
2018-10-13 21:45:42 -0700

    fixed heroku issue

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -66,12 +66,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  def vote
-    value = params[:type] == "up" ? 1 : -1
-    @message.add_or_update_evaluation(:votes, value, current_user)
-    redirect_to :back, notice: "Thank you for voting!"
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_message
